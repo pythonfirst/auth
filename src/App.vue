d53a0482b55d5a7b244658474a89786cca196f8d
zhaocanxiang <zhaocanxiang@bytedance.com>
2021-01-28 23:19:02 +0800

    add submenu

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <sideMenu></sideMenu>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+import sideMenu from './components/sideMenu.vue';
+
+export default {
+  components: { sideMenu },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
