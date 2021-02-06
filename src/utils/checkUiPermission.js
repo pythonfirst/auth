da1735f25febaddd70ad3aa70f03d3145d002f03
zhaocanxiang <zhaocanxiang@bytedance.com>
2021-02-06 23:19:59 +0800

    add comment

--- src/utils/checkUiPermission.js
+++ src/utils/checkUiPermission.js
@@ -5,7 +5,10 @@ export function getCurrentAuthority() {
   return [1|2|4|8|16|32|64|128 ];
 }
 
-
+/**
+ * 判断是否有权限
+ * @param {Array} key 功能点的权限标识
+ */
 export function check(key) {
   if (key === undefined) return true;
   const currentAuthority = getCurrentAuthority();
